064279ecc2945ab1b884179c0ba145bf3ae26460
Per Nordlöw <per.nordlow@gmail.com>
2025-08-25 19:55:24 +0200

    Modify source/app.d, source/waveform.d, source/waves.d

--- source/waveform.d
+++ source/waveform.d
@@ -7,33 +7,3 @@ import std.random : Random, uniform;
 
 import raylib : Wave;
 
-struct Static {
-	float f;
-	Wave generate() {
-		return typeof(return)();
-	}
-}
-
-struct Bounce {
-	float fS; // start frequency
-	float fE; // end frequency
-	Wave generate() {
-		return typeof(return)();
-	}
-}
-
-struct Boing {
-	float fS; // start frequency
-	float fE; // end frequency
-	Wave generate() {
-		return typeof(return)();
-	}
-}
-
-struct GlassBreak {
-	float fS; // start frequency
-	float fE; // end frequency
-	Wave generate() {
-		return typeof(return)();
-	}
-}
