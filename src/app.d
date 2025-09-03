f4466f01877c781a21422448883073edf73219b0
Per Nordlöw <per.nordlow@gmail.com>
2025-09-03 10:20:04 +0200

    Modify src/app.d

--- src/app.d
+++ src/app.d
@@ -169,7 +169,7 @@ void main() @trusted {
 						&& ball.position.y + ball.radius >= brick.position.y
 						&& ball.position.y - ball.radius
 						<= brick.position.y + brick.size.y) {
-						brick.flash();
+						brick.startFlashing();
 						ball.velocity.y *= -1;
 						PlaySound(brickSound);
 						break;
@@ -195,7 +195,7 @@ void main() @trusted {
 							&& bullet.position.y + bullet.radius >= brick.position.y
 							&& bullet.position.y - bullet.radius
 							<= brick.position.y + brick.size.y) {
-							flash(brick); // Start flashing
+							startFlashing(brick); // Start flashing
 							bullet.active = false;
 							PlaySound(brickSound);
 							break;
@@ -314,8 +314,7 @@ struct Brick/+Tegelsten+/ {
 
 static immutable float FLASH_DURATION = 0.3f;
 
-// Start the flashing effect on a brick.
-void flash(ref Brick brick) {
+void startFlashing(ref Brick brick) {
 	brick.isFlashing = true;
 	brick.flashTimer = 0.0f;
 }
