89a3ac565df7aea4f3f44a0ecec7a9eac62293e2
Per Nordlöw <per.nordlow@gmail.com>
2025-09-05 15:35:29 +0200

    Modify src/aliases.d

--- src/aliases.d
+++ src/aliases.d
@@ -7,5 +7,5 @@ import nxt.geometry;
 alias Pos2 = Point!(float, 2);
 alias Dim2 = Point!(float, 2); // TODO: Add Dimension/Size whatever to `nxt.geometry`
 alias Vec2 = Vector!(float, 2, false);
-alias NVec2 = Vector!(float, 2, true);
+alias Dir2 = Vector!(float, 2, true);
 alias Rect = Rectangle!(float);
