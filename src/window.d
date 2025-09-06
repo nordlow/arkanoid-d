85be0b68377c4e74cdb22b293a11ed28fa29f449
Per Nordlöw <per.nordlow@gmail.com>
2025-09-06 20:10:22 +0200

    Modify src/window.d

--- src/window.d
+++ src/window.d
@@ -1,4 +1,17 @@
 module window;
 
+import sdl3;
+
 @safe:
 
+struct Window {
+	@disable this(this);
+	this(in ScreenSize ssz) {
+	}
+	ScreenSize size() @property @trusted {
+		typeof(return) ssz;
+		SDL_GetWindowSize(window, &ssz.width, &ssz.height);
+		return ssz;
+	}
+	SDL_Window* window;
+}
