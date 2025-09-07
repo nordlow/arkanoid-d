9ccdcaa1c6446bb4c58ceabe49c1136b814f8e92
Per Nordlöw <per.nordlow@gmail.com>
2025-09-07 18:45:58 +0200

    Modify ci.sh, test.sh

--- test.sh
+++ test.sh
@@ -2,10 +2,4 @@
 
 set -euo pipefail
 
-echo -e "\n# Testing ..."
-
-echo -e "\n## Testing with DMD ..."
-dub -q test --compiler=dmd
-
-echo -e "\n## Testing with LDC using AddressSanitizer (ASan) ..."
-dub -q test --compiler=ldc2
+LD_LIBRARY_PATH="${HOME}"/.local/sdl-snapshot/lib exec dub -q run --compiler=dmd
