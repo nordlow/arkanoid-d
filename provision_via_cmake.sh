fbf29705f92550e38ad9c9980d11af50e13d3520
Per Nordlöw <per.nordlow@gmail.com>
2025-09-07 19:14:19 +0200

    Modify provision_via_cmake.sh, run.sh

--- provision_via_cmake.sh
+++ provision_via_cmake.sh
@@ -8,6 +8,8 @@ pushd SDL
 mkdir -p build
 pushd build
 
+DST_PREFIX=${HOME}/.local
+
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="${DST_PREFIX}/sdl-snapshot" ..
 cmake --build . --config Release --parallel
 cmake --install . --config Release
