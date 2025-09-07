42782874493ef300260de3c221104553c1cc79da
Per Nordlöw <per.nordlow@gmail.com>
2025-09-07 19:17:01 +0200

    Modify provision_via_cmake.sh

--- provision_via_cmake.sh
+++ provision_via_cmake.sh
@@ -2,7 +2,12 @@
 
 set -euo pipefail
 
-git clone https://github.com/libsdl-org/SDL.git
+if [ ! -d "SDL/.git" ]; then
+  echo "Cloning SDL..."
+  git clone https://github.com/libsdl-org/SDL.git
+else
+  git -C SDL pull
+fi
 
 pushd SDL
 mkdir -p build
@@ -10,7 +15,7 @@ pushd build
 
 DST_PREFIX=${HOME}/.local
 
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="${DST_PREFIX}/sdl-snapshot" ..
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="${DST_PREFIX}/sdl-snapshot" -DBUILD_SHARED_LIBS=ON -DBUILD_STATIC_LIBS=ON ..
 cmake --build . --config Release --parallel
 cmake --install . --config Release
 
