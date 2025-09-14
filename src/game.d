7a0ebdecd1d434c88e1905c669ee8b014646b507
Per Nordlöw <per.nordlow@gmail.com>
2025-09-14 19:07:20 +0200

    Modify src/game.d

--- src/game.d
+++ src/game.d
@@ -18,7 +18,7 @@ import joystick;
 nothrow struct Game {
 	import std.random : Random, unpredictableSeed;
 	@disable this(this);
-	this(in ScreenSize ssz, const uint ballCount = 30) @trusted {
+	this(in ScreenSize ssz, const uint ballCount = 1) @trusted {
 		this.ssz = ssz;
 		this.win = Window(ssz, "Arkanoid Clone", fullscreen: true);
 		joystick = openDefaultJoystick();
