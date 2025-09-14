ff256e8ff00415008fb74f41af1652fbac444bcf
Per Nordlöw <per.nordlow@gmail.com>
2025-09-14 11:46:24 +0200

    Add src/sdl/audio.d

---
+++ src/sdl/audio.d
@@ -0,0 +1,8 @@
+/++ SDL Audio.
+	See_Also: https://talesm.github.io/SDL3pp/group__CategoryAudio.html
+	See_Also: https://talesm.github.io/SDL3pp/SDL3pp__audio_8h_source.html
+ +/
+module sdl.audio;
+
+@safe:
+
