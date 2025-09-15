dd66dc0598472695eb2c107053e1593bd927f6b0
Per Nordlöw <per.nordlow@gmail.com>
2025-09-15 09:27:20 +0200

    Modify src/sdl/window.d

--- src/sdl/window.d
+++ src/sdl/window.d
@@ -8,7 +8,9 @@ alias DisplayMode = SDL_DisplayMode;
 
 nothrow struct Window {
 	alias Flags = SDL_WindowFlags;
+
 	@disable this(this);
+
 	this(in ScreenSize ssz, in char[] title, Flags flags = 0) @trusted {
 		/+ if (fullscreen) +/
 		/+	flags |= SDL_WINDOW_FULLSCREEN_DESKTOP; +/
@@ -21,24 +23,31 @@ nothrow struct Window {
 		fullscreen = true;
 		rdr = Renderer(this);
 	}
+
 	~this() nothrow @nogc @trusted @il
 		=> SDL_DestroyWindow(_ptr);
+
 	ScreenSize size() const scope @property @trusted @il  {
 		typeof(return) ssz;
 		SDL_GetWindowSize((cast()this)._ptr, &ssz.width, &ssz.height);
 		return ssz;
 	}
+
 	bool fullscreen() const scope nothrow @nogc @property @il
 		=> inFullscreen;
+
 	void fullscreen(in bool fullscreen_) scope @property @trusted @il {
 		if (!SDL_SetWindowFullscreen((cast()this)._ptr, fullscreen_))
 			warning("Couldn't set fullscreen state of %s to %s, %s", _ptr, fullscreen_, SDL_GetError());
 		inFullscreen = fullscreen_;
 	}
+
 	DisplayMode* fullscreenMode() scope @property @trusted @il
 		=> SDL_GetWindowFullscreenMode((cast()this)._ptr);
+
 	Flags flags() const scope @property @trusted @il
 		=> SDL_GetWindowFlags((cast()this)._ptr);
+
 	Renderer rdr;
 	package SDL_Window* _ptr;
 	invariant(_ptr);
