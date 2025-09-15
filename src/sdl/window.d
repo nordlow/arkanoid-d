2ae1a78c382f02bdc4fdbfb5b566e996d3ebe3b1
Per Nordlöw <per.nordlow@gmail.com>
2025-09-15 09:05:53 +0200

    Modify src/sdl/window.d

--- src/sdl/window.d
+++ src/sdl/window.d
@@ -22,7 +22,7 @@ nothrow struct Window {
 		rdr = Renderer(this);
 	}
 	~this() nothrow @nogc @trusted
-		=> SDL_DestroyWindow(_ptr);
+	=> SDL_DestroyWindow(_ptr);
 	ScreenSize size() const scope @property @trusted {
 		typeof(return) ssz;
 		SDL_GetWindowSize((cast()this)._ptr, &ssz.width, &ssz.height);
