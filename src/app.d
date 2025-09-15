555fd37b9f89659b8e51e8b29ee92abfb7dfa62f
Per Nordlöw <per.nordlow@gmail.com>
2025-09-15 09:47:21 +0200

    Modify src/app.d

--- src/app.d
+++ src/app.d
@@ -63,8 +63,10 @@ void main(string[] args) @trusted {
 		const bool rightHeld = keyStates[SDL_SCANCODE_RIGHT] != 0;
 
 		if (!game.over && !game.won) {
-			void moveLeft() => game.scene.paddle.moveLeft(deltaTime, ssz);
-			void moveRight() => game.scene.paddle.moveRight(deltaTime, ssz);
+			void moveLeft()
+				=> game.scene.paddle.moveLeft(deltaTime, ssz);
+			void moveRight()
+				=> game.scene.paddle.moveRight(deltaTime, ssz);
 			if (game.joystick.isValid) {
 				while (const ev = game.joystick.tryNextEvent()) {
 					tracef("Read %s heldButtons:", game.joystick.getHeldButtons);
