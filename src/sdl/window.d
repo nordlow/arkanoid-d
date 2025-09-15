9cfe5c7dd769ad066272a2ddd19f02e234530ad7
Per Nordlöw <per.nordlow@gmail.com>
2025-09-15 09:25:53 +0200

    Modify src/sdl/window.d

--- src/sdl/window.d
+++ src/sdl/window.d
@@ -10,17 +10,15 @@ nothrow struct Window {
 	alias Flags = SDL_WindowFlags;
 	@disable this(this);
 	this(in ScreenSize ssz, in char[] title, Flags flags = 0) @trusted {
-		inFullscreen = true;
 		/+ if (fullscreen) +/
 		/+	flags |= SDL_WINDOW_FULLSCREEN_DESKTOP; +/
 		_ptr = SDL_CreateWindow(title.ptr, ssz.width, ssz.height, cast(uint)flags);
-		if (inFullscreen)
-			SDL_SetWindowFullscreen(_ptr, true);
 		if (_ptr is null) {
 			warningf("Couldn't create window, %s", SDL_GetError().fromStringz);
 			SDL_Quit();
 			return;
 		}
+		fullscreen = true;
 		rdr = Renderer(this);
 	}
 	~this() nothrow @nogc @trusted
