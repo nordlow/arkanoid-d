acfcd5ca92d3eb211c03432cc8eb608cb7849001
Per Nordlöw <per.nordlow@gmail.com>
2025-09-15 11:22:21 +0200

    Modify src/sdl/renderer.d

--- src/sdl/renderer.d
+++ src/sdl/renderer.d
@@ -10,37 +10,42 @@ import sdl;
 
 struct Renderer { nothrow:
 	import std.math : sin, cos, PI;
-
+@il:
 	@disable this(this);
-
 	this(scope ref Window win, immutable char[] name = null) @trusted {
-		_ptr = SDL_CreateRenderer(win._ptr, name.ptr);
-		if (_ptr is null) {
+		auto ptr = SDL_CreateRenderer(win._ptr, name.ptr);
+		if (ptr is null) {
 			warningf("Couldn't create renderer, %s", SDL_GetError.fromStringz());
 			SDL_Quit();
 		}
-		if (!SDL_SetRenderVSync(_ptr, 1))
-			warning("VSync not supported");
-		initTabs();
+		this(ptr);
 	}
 
-	package this(scope ref SDL_Renderer* ptr) @trusted {
+	package this(scope ref SDL_Renderer* ptr) @trusted in(ptr) {
 		_ptr = ptr;
+		setVSync(1);
 		initTabs();
 	}
 
-	private void initTabs() scope pure nothrow @nogc @il {
+	private void initTabs() scope pure nothrow @nogc {
 		_sincos = SinCos(0, 2*PI);
 		_sincos.fill();
 	}
 
-	~this() @nogc @trusted @il
+	bool setVSync(in int vsync) @trusted {
+		const ret = SDL_SetRenderVSync(_ptr, 1);
+		if (!ret)
+			warning("VSync not supported");
+		return ret;
+	}
+
+	~this() @nogc @trusted
 		=> SDL_DestroyRenderer(_ptr);
 
-	int setDrawColor(in RGBA color) nothrow @nogc @trusted @il
+	int setDrawColor(in RGBA color) nothrow @nogc @trusted
 		=> SDL_SetRenderDrawColor(_ptr, color.r, color.g, color.b, color.a);
 
-	int fillRect(in SDL_FRect frect) nothrow @nogc @trusted @il
+	int fillRect(in SDL_FRect frect) nothrow @nogc @trusted
 		=> SDL_RenderFillRect(_ptr, &frect);
 
 	SDL_Renderer* _ptr;
