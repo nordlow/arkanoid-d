b4df2a74b886d6c6c9cb4296e2a3ef25256094f9
Per Nordlöw <per.nordlow@gmail.com>
2025-09-19 08:38:44 +0200

    Modify dub.sdl, run.sh

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 
 set -euo pipefail
 
-DST_PREFIX=${HOME}/.local
-LD_LIBRARY_PATH="${DST_PREFIX}"/sdl-snapshot/lib exec dub -q run --compiler=dmd
+DST_PREFIX=SDL/build
+LD_LIBRARY_PATH="${DST_PREFIX}" exec dub -q run --compiler=dmd
